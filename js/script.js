0f1adcaefc5da38f72e0540d55265b7efd896e8c
eg <eringoncer@gmail.com>
2016-03-25 14:21:28 -0700

    add more styling and elements to html.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-  $("#next").hide().fadeIn(1600);
-  $("img.arrow1").hide().fadeIn(1600);
+  $("#next").hide().fadeIn(2600);
+  $("img.arrow1").hide().fadeIn(2600);
 
   $("#survey").submit(function(event) {
 
@@ -16,8 +16,20 @@ $(document).ready(function() {
     } else if (passport === "Yes" && party === "No") {
       $(".patagonia").show();
       $(".section1").hide();
+    } else if (passport === "No" && party === "No") {
+      $(".home").show();
+      $(".section1").hide();
+    } else if (passport === "Yes" && nature === "Nature") {
+      $(".patagonia").show();
+      $(".section1").hide();
     } else if (passport === "No" && party === "Yes") {
       $(".vegas").show();
+      $(".section1").hide();
+    } else if (nature === "No" && solo === "Group") {
+      $(".tokyo").show();
+      $(".section1").hide();
+    } else if (solo === "Solo" && party === "No") {
+      $(".home").show();
       $(".section1").hide();
     } else {
       $(".home").show();
